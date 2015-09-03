a0c1511846d37d4f893d0d684d5c70d6bda6f47d
Tim Lochmüller <tim.lochmueller@hdnet.de>
2015-09-03 11:50:21 +0200

    Add wizard and folder icon

--- ext_tables.php
+++ ext_tables.php
@@ -13,3 +13,27 @@ $loader = array(
 \TYPO3\CMS\Extbase\Utility\ExtensionUtility::registerPlugin($_EXTKEY, 'Faq', 'FAQ');
 \TYPO3\CMS\Extbase\Utility\ExtensionUtility::registerPlugin($_EXTKEY, 'FaqTeaser', 'FAQ Teaser');
 \TYPO3\CMS\Extbase\Utility\ExtensionUtility::registerPlugin($_EXTKEY, 'FaqEnter', 'FAQ Eingabe');
+
+$GLOBALS['TCA']['pages']['columns']['module']['config']['items'][$_EXTKEY]['0'] = 'LLL:EXT:faq/Resources/Private/Language/locallang.xml:sysfolder';
+$GLOBALS['TCA']['pages']['columns']['module']['config']['items'][$_EXTKEY]['1'] = $_EXTKEY;
+
+\TYPO3\CMS\Backend\Sprite\SpriteManager::addTcaTypeIcon('pages', 'contains-faq',
+    \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::extRelPath($_EXTKEY) . 'Resources/Public/Icons/QuestionFolder.png');
+
+\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPageTSConfig('
+mod {
+	wizards.newContentElement.wizardItems.plugins {
+		elements {
+			faq {
+				icon = EXT:faq/Resources/Public/Icons/Question.png
+				title = FAQ
+				description = Create FAQ Plugin to handle the FAQ output
+				tt_content_defValues {
+					CType = list
+					list_type = faq_faq
+				}
+			}
+		}
+	}
+}
+');
