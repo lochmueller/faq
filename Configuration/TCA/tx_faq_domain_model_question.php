fd903656827a6ab0aa58472de1c52212e2bdaeef
Tim Lochmüller <tim@fruit-lab.de>
2015-11-17 20:17:22 +0100

    Add renderType configuration

--- Configuration/TCA/tx_faq_domain_model_question.php
+++ Configuration/TCA/tx_faq_domain_model_question.php
@@ -2,30 +2,31 @@
 $base = \HDNET\Autoloader\Utility\ModelUtility::getTcaInformation('HDNET\\Faq\\Domain\\Model\\Question');
 
 $custom = array(
-	'ctrl'    => array(
-		'sortby' => NULL,
-	),
-	'columns' => array(
-		'answer'     => array(
-			'config'        => array(
-				'type' => 'text'
-			),
-			'defaultExtras' => 'richtext:rte_transform[flag=rte_enabled|mode=ts_css]',
-		),
-		'categories' => array(
-			'config' => array(
-				'type'          => 'select',
-				'foreign_table' => 'tx_faq_domain_model_questioncategory',
-				'maxitems'      => '999',
-				'minitems'      => '1',
-				'MM'            => 'tx_faq_mm_question_questioncategory',
-				'renderMode'    => 'tree',
-				'treeConfig'    => array(
-					'parentField' => 'parent',
-				),
-			),
-		),
-	),
+    'ctrl'    => array(
+        'sortby' => null,
+    ),
+    'columns' => array(
+        'answer'     => array(
+            'config'        => array(
+                'type' => 'text'
+            ),
+            'defaultExtras' => 'richtext:rte_transform[flag=rte_enabled|mode=ts_css]',
+        ),
+        'categories' => array(
+            'config' => array(
+                'type'          => 'select',
+                'renderType'    => 'selectTree',
+                'foreign_table' => 'tx_faq_domain_model_questioncategory',
+                'maxitems'      => '999',
+                'minitems'      => '1',
+                'MM'            => 'tx_faq_mm_question_questioncategory',
+                'renderMode'    => 'tree',
+                'treeConfig'    => array(
+                    'parentField' => 'parent',
+                ),
+            ),
+        ),
+    ),
 );
 
 return \HDNET\Autoloader\Utility\ArrayUtility::mergeRecursiveDistinct($base, $custom);
--- Configuration/TCA/tx_faq_domain_model_questioncategory.php
+++ Configuration/TCA/tx_faq_domain_model_questioncategory.php
@@ -2,20 +2,21 @@
 $base = \HDNET\Autoloader\Utility\ModelUtility::getTcaInformation('HDNET\\Faq\\Domain\\Model\\Questioncategory');
 
 $custom = array(
-	'columns' => array(
-		'parent' => array(
-			'config' => array(
-				'type'          => 'select',
-				'foreign_table' => 'tx_faq_domain_model_questioncategory',
-				'maxitems'      => '1',
-				'minitems'      => '0',
-				'renderMode'    => 'tree',
-				'treeConfig'    => array(
-					'parentField' => 'parent',
-				),
-			),
-		),
-	),
+    'columns' => array(
+        'parent' => array(
+            'config' => array(
+                'type'          => 'select',
+                'renderType'    => 'selectTree',
+                'foreign_table' => 'tx_faq_domain_model_questioncategory',
+                'maxitems'      => '1',
+                'minitems'      => '0',
+                'renderMode'    => 'tree',
+                'treeConfig'    => array(
+                    'parentField' => 'parent',
+                ),
+            ),
+        ),
+    ),
 );
 
 return \HDNET\Autoloader\Utility\ArrayUtility::mergeRecursiveDistinct($base, $custom);
