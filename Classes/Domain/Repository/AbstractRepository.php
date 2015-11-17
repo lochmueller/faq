4f2f6b916c23b3e742c66caf94401dab1991914c
Tim Lochmüller <tim@fruit-lab.de>
2015-11-17 20:25:11 +0100

    Add missing repository function

--- Classes/Domain/Repository/AbstractRepository.php
+++ Classes/Domain/Repository/AbstractRepository.php
@@ -19,4 +19,22 @@ use TYPO3\CMS\Extbase\Persistence\Repository;
 abstract class AbstractRepository extends Repository
 {
 
+    /**
+     * Find objects by the given ids in the given order
+     *
+     * @param array $uids
+     *
+     * @return array
+     */
+    public function findByUidsSorted(array $uids)
+    {
+        $return = [];
+        foreach ($uids as $uid) {
+            $obj = $this->findByUid($uid);
+            if (is_object($obj)) {
+                $return[] = $obj;
+            }
+        }
+        return $return;
+    }
 }
