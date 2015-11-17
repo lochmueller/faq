c76750f5e18d4bba8b99385b3e20461f5413580c
Tim Lochmüller <tim@fruit-lab.de>
2015-11-17 20:29:44 +0100

    Add missing getTableName method

--- Classes/Domain/Repository/AbstractRepository.php
+++ Classes/Domain/Repository/AbstractRepository.php
@@ -9,6 +9,8 @@
 
 namespace HDNET\Faq\Domain\Repository;
 
+use Exception;
+use TYPO3\CMS\Extbase\Persistence\Generic\Query;
 use TYPO3\CMS\Extbase\Persistence\Repository;
 
 /**
@@ -18,6 +20,25 @@ use TYPO3\CMS\Extbase\Persistence\Repository;
  */
 abstract class AbstractRepository extends Repository
 {
+
+    /**
+     * Return the current tablename
+     *
+     * @throws Exception
+     * @return string
+     */
+    public function getTableName()
+    {
+        $query = $this->createQuery();
+        if ($query instanceof Query) {
+            $source = $query->getSource();
+            if (method_exists($source, 'getSelectorName')) {
+                return $source->getSelectorName();
+            }
+        }
+
+        throw new Exception('Can\'t get the table name of the current object', 123671823123);
+    }
 
     /**
      * Find objects by the given ids in the given order
