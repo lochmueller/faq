d825697e37b0ce0b39f567206d7d61ad72ea40c6
Martin Horniak <m.horniak@sineos.de>
2020-05-07 20:34:02 +0200

    [TASK] Resolve missing $_EXTCONF in TCA override for pages

--- Configuration/TCA/Overrides/pages.php
+++ Configuration/TCA/Overrides/pages.php
@@ -2,9 +2,9 @@
 
 declare(strict_types = 1);
 
-$GLOBALS['TCA']['pages']['columns']['module']['config']['items'][$_EXTKEY] = [
+$GLOBALS['TCA']['pages']['columns']['module']['config']['items'][] = [
     'LLL:EXT:faq/Resources/Private/Language/locallang.xlf:sysfolder',
-    $_EXTKEY,
+    'faq',
     'contains-faq',
 ];
 $GLOBALS['TCA']['pages']['ctrl']['typeicon_classes']['contains-faq'] = 'apps-pagetree-folder-contains-faq';
