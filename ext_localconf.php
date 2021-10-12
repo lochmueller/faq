998cee53ccac7aa859784ba4e33f64d910afc7e8
Lukas Schönbeck <lukas.schoenbeck@hdnet.de>
2021-10-12 09:27:46 +0200

    Remove comment configurations and fix location of extension icon

--- ext_localconf.php
+++ ext_localconf.php
@@ -6,17 +6,3 @@
     'faq',
     \HDNET\Faq\Utility\ExtensionUtility::getAutoloaderConfiguration()
 );
-
-//\TYPO3\CMS\Extbase\Utility\ExtensionUtility::configurePlugin(
-//    'faq',
-//    'Faq',
-//    [\HDNET\Faq\Controller\FaqController::class => 'index'],
-//    [\HDNET\Faq\Controller\FaqController::class => 'index']
-//);
-//
-//\TYPO3\CMS\Extbase\Utility\ExtensionUtility::configurePlugin(
-//    'faq',
-//    'FaqEnter',
-//    [\HDNET\Faq\Controller\FaqController::class => 'form,send,user,thanks,submit'],
-//    [\HDNET\Faq\Controller\FaqController::class => 'send,user,thanks,submit']
-//);
